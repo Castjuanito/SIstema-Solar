814ff5a37289bc731a933af2338e3f46158e2a4d
Castjuanito <juanario1997@gmail.com>
2019-03-07 16:11:48 -0500

    animacion del sol

--- taller_Sistema_Solar/client/js/sistema_solar.js
+++ taller_Sistema_Solar/client/js/sistema_solar.js
@@ -12,25 +12,25 @@ controls.maxDistance = 200;
 
 
 var mercury = crearPlaneta(0.7,24,24,'images/mercuryTexture.png',12,0,0);
-hacerAnillo(20, 0.05, 480, 0x757064, 0);
+hacerAnillo(20, 0.07, 480, 0x757064, 0);
 var venus = crearPlaneta(2,24,24,'images/venusTextura.jpg',12,0,0);
-hacerAnillo(30, 0.05, 480, 0x757064, 0);
+hacerAnillo(30, 0.07, 480, 0x757064, 0);
 var earth = crearPlaneta(2, 24, 24, 'images/tierraTextura.jpg', 12, 0, 0);
-hacerAnillo(42, 0.05, 480, 0x757064, 0);
+hacerAnillo(42, 0.07, 480, 0x757064, 0);
 var moon = crearPlaneta(0.27,24,24,'images/lunaTextura.jpeg',12,2,0);
 
 var marte = crearPlaneta(1.2,24,24,'images/marteTextura.jpg',12,0,0);
-hacerAnillo(55, 0.05, 480, 0x757064, 0);
+hacerAnillo(55, 0.07, 480, 0x757064, 0);
 var jupiter = crearPlaneta(8,24,24,'images/jupiterTexture.jpg',12,0,0);
-hacerAnillo(73, 0.05, 480, 0x757064, 0);
+hacerAnillo(73, 0.07, 480, 0x757064, 0);
 var saturn = crearPlaneta(6,24,24,'images/saturnTextura.jpeg',12,0,0);
-hacerAnillo(100, 0.05, 480, 0x757064, 0);
+hacerAnillo(100, 0.07, 480, 0x757064, 0);
 var uranus = crearPlaneta(4.5,24,24,'images/uranusTextura.jpeg',12,0,0);
-hacerAnillo(125, 0.05, 480, 0x757064, 0);
+hacerAnillo(125, 0.07, 480, 0x757064, 0);
 var neptune = crearPlaneta(4.3,24,24,'images/neptuneTextura.jpg',12,0,0);
-hacerAnillo(150, 0.05, 480, 0x757064, 0);
+hacerAnillo(150, 0.07, 480, 0x757064, 0);
 var pluto = crearPlaneta(0.3,24,24,'images/plutoTextura.jpeg',12,0,0);
-hacerAnillo(140, 0.05, 480, 0x757064, 0);
+hacerAnillo(140, 0.07, 480, 0x757064, 0);
 
 
 
@@ -39,12 +39,27 @@ var textureSol = new THREE.TextureLoader().load('images/solTextura.jpeg');
 var materialSol = new THREE.MeshBasicMaterial({ map: textureSol });
 var sun = new THREE.Mesh(geometrySol, materialSol);
 
+var sphereGeom =  new THREE.SphereGeometry( 17, 10, 16 );
+var amarilloMaterial = new THREE.MeshBasicMaterial( { color: 0xff9900, transparent: true, opacity: 0.5 } );
+var brilloAmarillo = new THREE.Mesh( sphereGeom, amarilloMaterial );
+
+var sphereGeom1 =  new THREE.SphereGeometry( 17, 10, 16 );
+var rojoMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, transparent: true, opacity: 0.5 } );
+var brillorojo = new THREE.Mesh( sphereGeom1, rojoMaterial );
+
+var sphereGeom2 =  new THREE.SphereGeometry( 17, 10, 16 );
+var naranjaMaterial = new THREE.MeshBasicMaterial( { color: 0xff6600, transparent: true, opacity: 0.5 } );
+var brillonaranja = new THREE.Mesh( sphereGeom2, naranjaMaterial );
+
 light = new THREE.PointLight(0xb4e7f2, 1.5);
 light.angle = Math.PI / 2;
 light.position.set(0, 0, 0);
 scene.add(light)
 
 scene.add(sun);
+scene.add(brillorojo);
+scene.add(brilloAmarillo);
+scene.add(brillonaranja);
 time = 0;
 
 
@@ -74,7 +89,14 @@ var render = function () {
     sun.rotation.x += 0.0;
     sun.rotation.y += 0.01;
 
+    brillorojo.rotation.x -= 0.05;
+    brillorojo.rotation.y -= 0.01;
 
+    brilloAmarillo.rotation.x -= 0.01;
+    brilloAmarillo.rotation.y -= 0.06;
+
+    brillonaranja.rotation.x += 0.01;
+    brillonaranja.rotation.z += 0.06;
 
     mercury.position.x= 20 * Math.cos(time*2);
     mercury.position.y = 20 * Math.sin(time*2);
